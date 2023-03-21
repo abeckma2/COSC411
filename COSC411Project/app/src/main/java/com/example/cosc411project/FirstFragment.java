498ece79b48a7e86986033362e291e8dac506119
abeckma2 <abeckma2@emich.edu>
2023-03-20 22:22:14 -0400

    Add files via upload

--- COSC411Project/app/src/main/java/com/example/cosc411project/FirstFragment.java
+++ COSC411Project/app/src/main/java/com/example/cosc411project/FirstFragment.java
@@ -4,8 +4,23 @@ import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.content.Context;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 
 public class FirstFragment extends AppCompatActivity {
@@ -21,12 +36,29 @@ public class FirstFragment extends AppCompatActivity {
     {
         finish();
     }
+    final RelativeLayout lin=(RelativeLayout) findViewById(R.id.linear);
     public void makeViews(int v)
     {
-        TextView text[]=new TextView[v];
+        TextView words[]=new TextView[v];
         for(int i=0;i<v;i++)
         {
-            text[i]=new TextView(this);
+            words[i]=new TextView(this);
+            words[i].setId(TextView.generateViewId());
+            words[i].setBackgroundColor(Color.BLACK);
+            words[i].setTextColor(Color.parseColor("#000000"));
+            words[i].setTextSize(TypedValue.COMPLEX_UNIT_SP,30);
+            words[i].setGravity(Gravity.CENTER);
+            RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(0,0);
+            if(i>0)
+            {
+                params.addRule(RelativeLayout.BELOW,words[i-1].getId());
+            }
+            params.width= ViewGroup.LayoutParams.WRAP_CONTENT;
+            params.height=(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,50,getResources().getDisplayMetrics());
+            params.topMargin=1;
+            words[i].setLayoutParams(params);
+            lin.addView(words[i]);
+
         }
     }
 
--- COSC411Project/app/src/main/java/com/example/cosc411project/MainActivity.java
+++ COSC411Project/app/src/main/java/com/example/cosc411project/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.cosc411project;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.content.Context;
 import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -31,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         int i=3;
         makeButtons(i);
     }
+    final ScrollView scroll=(ScrollView)findViewById(R.id.scroll);
+    final RelativeLayout lin=(RelativeLayout) findViewById(R.id.linear);
     public void makeButtons(int number)
     {
         while(true)
@@ -39,7 +52,28 @@ public class MainActivity extends AppCompatActivity {
             for(int i=0;i<number;i++)
             {
                 button[i]=new Button(this);
+                button[i].setId(Button.generateViewId());
+                button[i].setBackgroundColor(Color.BLACK);
+                button[i].setTextColor(Color.parseColor("#009900"));
+                button[i].setTextSize(TypedValue.COMPLEX_UNIT_SP,30);
+                button[i].setGravity(Gravity.CENTER);
+
+                RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(0,0);
+                if(i>0)
+                {
+                    params.addRule(RelativeLayout.BELOW,button[i-1].getId());
+                }
+                params.width=params.MATCH_PARENT;
+                params.height=(int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,50,getResources().getDisplayMetrics());
+                params.topMargin=1;
+                button[i].setLayoutParams(params);
+                lin.addView(button[i]);
             }
         }
+    }
+    public void login(View view)
+    {
+        Intent secondActivity=new Intent(this,SecondFragment.class);
+        startActivity(secondActivity);
     }
 }
